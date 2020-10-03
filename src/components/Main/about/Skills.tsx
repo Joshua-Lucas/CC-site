bd963a4800e7f37b8133d43de51231e960ac5704
Joshua Lucas <joshuamlucas91@gmail.com>
2020-10-03 19:21:21 -0400

    started creating skills component

---
+++ src/components/Main/about/Skills.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//Styled Components
+
+//Interfaces
+
+//React Component
+const Skills: React.FC = () => {
+  return (
+    <>
+      <h1>How can we help?</h1>
+      {/* Options of what we can do? */}
+    </>
+  )
+}
+export default Skills
