ee3c27f0e73726677d7766a17d10ebc37cfa7eee
Joshua Lucas <joshuamlucas91@gmail.com>
2020-10-03 19:21:01 -0400

    added about section and connected to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Header from '../src/components/Header/Header'
 import Main from '../src/components/Main/Main'
 
--- src/components/Main/about/About.tsx
+++ src/components/Main/about/About.tsx
@@ -2,7 +2,58 @@ import React from 'react'
 import styled from 'styled-components'
 
 //Styled Components
-const AboutContainer = styled.div``
+const AboutContainer = styled.div`
+  margin: 0 0.75rem;
+  margin-top: 17rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (min-width: 786px) {
+    max-width: 70%;
+    margin: 17rem auto 0;
+    justify-content: center;
+    align-items: flex-end;
+  }
+`
+const Title = styled.h1`
+  text-align: center;
+  font-size: 3.5rem;
+  @media (min-width: 786px) {
+    text-align: right;
+    margin: 0;
+    font-size: 4rem;
+  }
+`
+const SubContainer = styled.div`
+  width: 75%;
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 786px) {
+    align-items: flex-end;
+  }
+`
+
+const Subtitle = styled.h2`
+  text-align: center;
+  font-size: 2.25rem;
+  font-weight: 500;
+  @media (min-width: 786px) {
+    text-align: none;
+    align-self: center;
+    margin: 2rem;
+  }
+`
+const AboutText = styled.p`
+  text-align: center;
+  line-height: 1.75;
+  @media (min-width: 786px) {
+    margin: 0;
+    text-align: right;
+    font-size: 1.25rem;
+    font-weight: 300;
+  }
+`
 //Interfaces
 
 //React Component
@@ -10,19 +61,17 @@ const About: React.FC = () => {
   return (
     <>
       <AboutContainer>
-        <h2>Hi, I am Joshua Lucas. Pleasure to meet you.</h2>
-        <p>
-          I am a self-taught fullstack developer who is passionate about
-          developing intuitive, responsive software with the user in mind. I am
-          driven, hardworking, honest, and a loyal team player. I am a quick
-          learner, perpetually seeking to sharpen my skills and grow in my
-          knowledge of software development.
-        </p>
-        <p>
-          When I am not at my computer, you can find me enjoying the outdoors
-          with my wife, trying a new restaurant, or conversing with a friend
-          over a cup of coffee.
-        </p>
+        <Title>Need help with buisness growth?</Title>
+        <SubContainer>
+          <Subtitle>Hi! I am Bronson</Subtitle>
+          <AboutText>
+            I have learned many lessons throughout my buiness career focusing on
+            operations and people development. I have a passion for people and
+            sustainable development bloomed while I worked as a General Manager
+            at a local firm Campus Quilt Company. I am here to share what I have
+            learned with you to make your buiness flurish in todays market.
+          </AboutText>
+        </SubContainer>
       </AboutContainer>
     </>
   )
