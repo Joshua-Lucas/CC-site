18c8e9e8d391ac70d9080f9717e6c1a1121016be
Joshua Lucas <joshuamlucas91@gmail.com>
2020-10-04 17:53:20 -0400

    Update head to acheive better SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Head from 'next/head'
 import Header from '../src/components/Header/Header'
 import Main from '../src/components/Main/Main'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Cordle Consulting</title>
+        <meta name="Description" content="Consulting information" key="Home" />
+      </Head>
       <Header />
       <Main />
     </>
