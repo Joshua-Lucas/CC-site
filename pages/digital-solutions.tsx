68e9ce8e331306405105802a741df0da20f73533
Joshua Lucas <joshuamlucas91@gmail.com>
2020-10-05 22:09:04 -0400

    add page for digital services

---
+++ pages/digital-solutions.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function digitalSolutions() {
+  return (
+    <>
+      <Head>
+        <title>Cordle Consulting</title>
+        <meta
+          name="Description"
+          content="Digital Solution Products"
+          key="Digital Solutions"
+        />
+      </Head>
+      <div>
+        <h1>Page coming soon</h1>
+      </div>
+    </>
+  )
+}
